16e0934b8ed0aa6063e571e96926d8ea16d4e421
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-08-19 22:27:11 +0100

    feat:defined their msg component

--- src/components/message/TheirMessage.jsx
+++ src/components/message/TheirMessage.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
-const TheirMessage = () => {
-  return (
-    <div>TheirMessage</div>
-  )
+const TheirMessage = ({message, lastMessage}) => {
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
+
+    return (
+      <div className="message-row">
+        {isFirstMessageByUser && (
+          <div
+            className="message-avatar"
+            style={{ backgroundImage: message.sender && `url(${message.sender.avatar})` }}
+          />
+        )}
+        {message.attachments && message.attachments.length > 0
+          ? (
+            <img
+              src={message.attachments[0].file}
+              alt="message-attachment"
+              className="message-image"
+              style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+            />
+          )
+          : (
+            <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
+              {message.text}
+            </div>
+          )}
+      </div>
+    );
 }
 
 export default TheirMessage;
