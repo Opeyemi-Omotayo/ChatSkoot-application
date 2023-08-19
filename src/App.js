cbe5e20414162fc4fb852185a92c058d45300785
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-08-19 20:52:51 +0100

    defining the chat engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { ChatEngine } from "react-chat-engine";
+
+const projectID = process.env.REACT_APP_PROJECT_ID
+const userSecret = process.env.REACT_APP_USERSECRET
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World </h1>
+      <ChatEngine 
+          height = "100vh"
+          projectID = {projectID} 
+          userName = "Opeyemi"
+          userSecret = {userSecret}
+      />
     </div>
   );
 }
