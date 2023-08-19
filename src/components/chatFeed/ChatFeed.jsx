7ac6b298bee0cfd589787dc55826bf7296bf83e7
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-08-19 22:07:00 +0100

    feat:rendering messages in the chat feed

--- src/components/chatFeed/ChatFeed.jsx
+++ src/components/chatFeed/ChatFeed.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import MyMessage from '../message/MyMessage';
+import TheirMessage from '../message/TheirMessage';
+import MessageForm from '../message/MessageForm';
 
 const ChatFeed = (props) => {
-    console.log(props);
+    const { chats, activeChat, userName, messages } = props;
+
+    const chat = chats && chats[activeChat];
+
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+    
+        return keys.map((key, index) => {
+          const message = messages[key];
+          const lastMessageKey = index === 0 ? null : keys[index - 1];
+          const isMyMessage = userName === message.sender.username;
+    
+          return (
+            <div key={`msg_${index}`} style={{ width: '100%' }}>
+              <div className="message-block">
+                {isMyMessage
+                  ? <MyMessage message={message} />
+                  : <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />}
+              </div>
+              <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
+              </div>
+            </div>
+          );
+        });
+      };
+
   return (
-    <div>ChatFeed</div>
+    <div className="chat-feed">
+      <div className="chat-title-container">
+        <div className="chat-title">{chat?.title}</div>
+        <div className="chat-subtitle">
+
+        </div>
+      </div>
+      {renderMessages()}
+      <div style={{ height: '100px' }} />
+      <div className="message-form-container">
+      <MessageForm {...props} chatId={activeChat} />
+      </div>
+    </div>
   )
 }
 
