d87deaac12e99a1fe33e89de1bb242833d2e248c
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-08-19 21:21:33 +0100

    added a chat feed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { ChatEngine } from "react-chat-engine";
+import ChatFeed from './components/chatFeed/ChatFeed';
 
 const projectID = process.env.REACT_APP_PROJECT_ID
 const userSecret = process.env.REACT_APP_USERSECRET
@@ -12,6 +13,9 @@ function App() {
           projectID = {projectID} 
           userName = "Opeyemi"
           userSecret = {userSecret}
+          renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+          onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+
       />
     </div>
   );
